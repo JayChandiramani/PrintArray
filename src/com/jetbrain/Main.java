76a246b96387d148ba32def52d68affc80dca37b
JayChandiramani <jaychandiramani2003@gmail.com>
2024-03-06 16:37:30 -0500

    Initial commit

---
+++ src/com/jetbrain/Main.java
@@ -0,0 +1,13 @@
+package com.jetbrain;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] Numbers = {5, 45, 72, 73, 90993672};
+
+	JOptionPane.showMessageDialog(null,"Number 1: "+Numbers[0]+"\nNumber 2: "+Numbers[1]+"\nNumber 3: "+Numbers[2]+"\nNumber 4: "+Numbers[3]+"\nNumber 5: "+Numbers[4]);
+	System.exit(0);
+    }
+}
